eb35d2047447bc8644063a80ab9d5cba5d06b45b
Borja Muñoz <bmunoz@carto.com>
2022-07-16 16:50:34 +0200

    Update layer style

--- app/src/components/layers/BuildingsLayer.js
+++ app/src/components/layers/BuildingsLayer.js
@@ -10,13 +10,77 @@ export default function BuildingsLayer() {
   const { buildingsLayer } = useSelector((state) => state.carto.layers);
   const source = useSelector((state) => selectSourceById(state, buildingsLayer?.source));
   const cartoLayerProps = useCartoLayerProps({ source });
+  const currentView = useSelector((state) => state.app.currentView);
 
   if (buildingsLayer && source) {
     return new CartoLayer({
       ...cartoLayerProps,
       id: BUILDINGS_LAYER_ID,
-      getFillColor: [241, 109, 122],
+      extruded: true,
+      getFillColor: (d) => {
+        if (currentView === 'construction-date') {
+          if (d.properties.construction_date < '1900-01-01') {
+            return [158, 1, 66];
+          } else if (d.properties.construction_date < '1920-01-01') {
+            return [213, 62, 79];
+          } else if (d.properties.construction_date < '1940-01-01') {
+            return [244, 109, 67];
+          } else if (d.properties.construction_date < '1950-01-01') {
+            return [253, 174, 97];
+          } else if (d.properties.construction_date < '1960-01-01') {
+            return [254, 224, 139];
+          } else if (d.properties.construction_date < '1970-01-01') {
+            return [255, 255, 191];
+          } else if (d.properties.construction_date < '1980-01-01') {
+            return [230, 245, 152];
+          } else if (d.properties.construction_date < '1990-01-01') {
+            return [171, 221, 164];
+          } else if (d.properties.construction_date < '2000-01-01') {
+            return [102, 194, 165];
+          } else if (d.properties.construction_date < '2010-01-01') {
+            return [50, 136, 189];
+          } else {
+            return [94, 79, 162];
+          }
+        } else {
+          // return colorCategories({
+          //   attr: 'current_use',
+          //   domain: [
+          //     '1_residential',
+          //     '2_agriculture',
+          //     '3_industrial',
+          //     '4_1_office',
+          //     '4_2_retail',
+          //     '4_3_publicServices',
+          //   ],
+          //   colors: 'Bold',
+          //   othersColor: [200, 200, 200]
+          // });
+          if (d.properties.current_use === '1_residential') {
+            return [57, 105, 172];
+          } else if (d.properties.current_use === '2_agriculture') {
+            return [17, 165, 121];
+          } else if (d.properties.current_use === '3_industrial') {
+            return [127, 60, 141];
+          } else if (d.properties.current_use === '4_1_office') {
+            return [242, 183, 1];
+          } else if (d.properties.current_use === '4_2_retail') {
+            return [231, 63, 116];
+          } else if (d.properties.current_use === '4_3_publicServices') {
+            return [128, 186, 90];
+          } else {
+            return [0, 134, 149];
+          }
+        }
+      },
+      getElevation: (d) => {
+        return d.properties.number_floors_above_ground * 3;
+      },
       pickable: true,
+      updateTriggers: {
+        getFillColor: [currentView],
+        ...cartoLayerProps.updateTriggers,
+      },
       onHover: (info) => {
         if (info?.object) {
           info.object = {
