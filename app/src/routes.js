83c29aaa102cb6c17994ea7834f8c28ee372986f
Borja Muñoz <borja.munoz@gmail.com>
2022-07-19 15:11:22 +0200

    Navigate by default to the buildings view

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,16 +1,19 @@
 import { lazy } from 'react';
 import ProtectedRoute from 'components/common/ProtectedRoute';
 import DefaultView from 'components/common/DefaultView';
+import { Navigate } from 'react-router-dom';
 
 const Main = lazy(() => import(/* webpackPrefetch: true */ 'components/views/main/Main'));
 const NotFound = lazy(() => import('components/views/NotFound'));
 const Login = lazy(() => import('components/views/Login'));
+const Buildings = lazy(() => import('components/views/Buildings.js'));
 // [hygen] Import views
 
 export const ROUTE_PATHS = {
   LOGIN: '/login',
   DEFAULT: '/',
   NOT_FOUND: '/404',
+  BUILDINGS: '/buildings',
   // [hygen] Add path routes
 };
 
@@ -25,7 +28,8 @@ const routes = [
       </ProtectedRoute>
     ),
     children: [
-      // { path: '/', element: <Navigate to='/<your default view>' /> },
+      { path: '/', element: <Navigate to='/buildings' /> },
+      { path: ROUTE_PATHS.BUILDINGS, element: <Buildings /> },
       // [hygen] Add routes
     ],
   },
